a8aa425cc001a6111a34aac25656a181133f31e0
QurayshSaif <saifquraysh@gmaill.com>
2023-03-04 15:52:41 -0500

    feature/list - added new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.scss";
 import VideoDetails from "./data/video-details.json";
+import Videos from "./data/videos.json";
 
 import Header from "./components/Header/header";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+import VideoList from "./components/VideoList/VideoList";
 
 function App() {
   return (
     <>
       <Header />
-      <VideoPlayer image={VideoDetails[0].image} />
+      <VideoPlayer image={Videos[0].image} />
+      <VideoList />
     </>
   );
 }
