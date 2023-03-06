69a3b870a4c0b55832b951728d8e6a6ca102762d
QurayshSaif <saifquraysh@gmaill.com>
2023-03-05 21:18:52 -0500

    Converted timestamp to date format

--- src/components/UserComments/UserComments.jsx
+++ src/components/UserComments/UserComments.jsx
@@ -1,6 +1,16 @@
 import "./UserComments.scss";
 
 export default function UserComments({ commentsList }) {
+  function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const formattedMonth = month < 10 ? `0${month}` : month;
+    const formattedDay = day < 10 ? `0${day}` : day;
+    const formattedDate = `${formattedMonth}/${formattedDay}/${year}`;
+    return formattedDate;
+  }
   return (
     <section className="comments">
       {commentsList.map((comment) => (
@@ -11,7 +21,9 @@ export default function UserComments({ commentsList }) {
           <div className="comments__main-container">
             <div className="comments__secondary-container">
               <h4 className="comments__name">{comment.name}</h4>
-              <span className="comments__date">{comment.timestamp}</span>
+              <span className="comments__date">
+                {formatDate(comment.timestamp)}
+              </span>
             </div>
             <div className="comments__third-container">
               <p className="comments__text">{comment.comment}</p>
--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -10,6 +10,17 @@ export default function VideoDetails({
   likes,
   description,
 }) {
+  // function that converts Unix timestamp to MM/DD/YYYY format
+  function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const formattedMonth = month < 10 ? `0${month}` : month;
+    const formattedDay = day < 10 ? `0${day}` : day;
+    const formattedDate = `${formattedMonth}/${formattedDay}/${year}`;
+    return formattedDate;
+  }
   return (
     <section className="video-details">
       <h1 className="video-details__title">{title}</h1>
@@ -18,7 +29,7 @@ export default function VideoDetails({
           <span className="video-details__text video-details__text--subtitle">
             By {channel}
           </span>
-          <span className="video-details__text">{timestamp}</span>
+          <span className="video-details__text">{formatDate(timestamp)}</span>
         </div>
         <div className="video-details__info-container">
           <div className="video-details__icon-container video-details__icon-container--view">
