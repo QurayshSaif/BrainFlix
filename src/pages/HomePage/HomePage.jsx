9927144d853d7a0aa61e122f00048aadad035e06
QurayshSaif <saifquraysh@gmaill.com>
2023-05-08 14:32:28 -0400

    added loading to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import "./HomePage.scss";
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Video from "../../components/Video/Video";
 import { API_URL } from "../../utils/api";
+import ReactLoading from "react-loading";
 
 function HomePage({ videos }) {
   const [activeVideo, setActiveVideo] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (videos && videos.length > 0) {
@@ -15,10 +16,15 @@ function HomePage({ videos }) {
         .get(`${API_URL}/${firstVideo.id}`)
         .then((result) => {
           setActiveVideo(result.data);
+          setLoading(false);
         })
         .catch((error) => console.error(error));
     }
   }, [videos]);
+
+  if (loading) {
+    return <ReactLoading type={"spin"} color={"#00bcd4"} />;
+  }
 
   return <Video activeVideo={activeVideo} videos={videos} />;
 }
